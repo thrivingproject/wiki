9c7ea5683f70998e0f1ea3cbadd8c285ae0d7d6e
thrivingproject <98602195+thrivingproject@users.noreply.github.com>
2022-02-18 17:32:27 -0400

    deleted empty lines, removed csrf token, alphabetized imports

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-import markdown2
 from django.urls import reverse
+import markdown2
 
 from . import util
 
@@ -21,14 +21,17 @@ def entry(request, page):
     except: 
         return render(request, "encyclopedia/page_not_found.html")
 
+# Search function for getting query from form
 def search(request):   
-    search = request.GET['q']
-    if(util.get_entry(search) is not None):
-        return HttpResponseRedirect(reverse("page", kwargs={'page': search }))
+    search_string = request.GET['q']
+    # Redirect to entry
+    if(util.get_entry(search_string) is not None):
+        return HttpResponseRedirect(reverse("page", kwargs={'page': search_string }))
+    # Display search_string results    
     else:
         results = []
         for i in util.list_entries():
-            if search.lower() in i.lower():
+            if search_string.lower() in i.lower():
                 results.append(i)
 
         return render(request, "encyclopedia/results.html", {

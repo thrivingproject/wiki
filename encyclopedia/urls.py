bb434eab91ddd4056d6c9846082d8c5f18a157a1
thrivingproject <98602195+thrivingproject@users.noreply.github.com>
2022-02-18 21:25:41 -0400

    renamed view_page view function from entry to view_page, changed search view function to call view_page function, added edit.html

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:page>/", views.entry, name="page"),
+    path("wiki/<str:search_string>/", views.view_page, name="page"),
     path("search", views.search, name="search"),
+    path("edit", views.edit, name="edit"),
+    #path("save", views.save, name="save")
 
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,23 +5,28 @@ import markdown2
 
 from . import util
 
+# Index
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-# /wiki/<page> function to use get_entry() to render page's markdown to HTML
-def entry(request, page):
+# Page
+def view_page(request, search_string):
+    # If page exists
     try:
-        content = markdown2.markdown(util.get_entry(page))
-        return render(request, "encyclopedia/entry.html", {
-            "title": page,
+        content = markdown2.markdown(util.get_entry(search_string))
+        return render(request, "encyclopedia/page.html", {
+            "title": search_string,
             "body": content
         })
+    # Does not exist
     except: 
-        return render(request, "encyclopedia/page_not_found.html")
+        return render(request, "encyclopedia/page_not_found.html", {
+            "search_string": search_string
+        })
 
-# Search function for getting query from form
+# Search
 def search(request):   
     search_string = request.GET['q']
     # Redirect to entry
@@ -30,10 +35,22 @@ def search(request):
     # Display search_string results    
     else:
         results = []
+        # Check if search string matches entries
         for i in util.list_entries():
             if search_string.lower() in i.lower():
                 results.append(i)
+        # Search results found        
+        if len(results):
+            return render(request, "encyclopedia/results.html", {
+            "entries": results
+            })
+        # No results found
+        else:
+            return view_page(request, search_string)
+        
 
-        return render(request, "encyclopedia/results.html", {
-        "entries": results
+# Edit
+def edit(request):
+    return render(request, "encyclopedia/edit.html", {
+        "entries": util.list_entries()
     })
